cd33e2aec43d296304ea1ec8166e3141bbed33b4
Fabian AUFFRAY <fabianauffray78@gmail.com>
2025-05-06 13:11:19 +0000

    Exos 3

---
+++ models/staging/gz_raw_data/stg_gz_raw_data__product.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'product') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        CAST(ship_cost AS INTEGER)
+
+    from source
+    
+
+)
+
+select * from renamed
