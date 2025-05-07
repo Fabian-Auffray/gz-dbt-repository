a264d6a704af33de1b7544b9e8f433d521d8faee
Fabian Auffray <fabianauffray78@gmail.com>
2025-05-07 14:10:09 +0000

    got stuff working again end of day 1 more or less

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -8,5 +8,5 @@ SELECT
   ROUND((quantity*p.purchase_price),2) AS purchase_cost,
   ROUND((revenue-(quantity*p.purchase_price)),2) AS margin
 FROM {{ ref('stg_raw__sales') }} as s 
- INNER JOIN {{ ref('stg_raw__product') }} as p
+ INNER JOIN  {{ ref('stg_raw__products') }} as p
     USING (products_id)
