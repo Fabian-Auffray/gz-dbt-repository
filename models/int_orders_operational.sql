d92134c346aaf4d07f8f4372e02bcf1b55441e4d
Fabian AUFFRAY <fabianauffray78@gmail.com>
2025-05-07 10:56:32 +0000

    Schéma

--- models/int_orders_operational.sql
+++ models/int_orders_operational.sql
@@ -1,13 +1,15 @@
-SELECT 
-  orders_id,
-  date_date,
-  ROUND(SUM(margin+o.shipping_fee)-logcost-ship_cost) as operational_margin 
-  quantity,
-  shipping_fee,
-  ship_cost,
-  revenue,
-  log_cost,
-  FROM {{ ref('int_sales_margin') }} as s 
-  INNER JOIN {{ ref('int_orders_margin') }} as o 
-  USING (orders_id)
-  GROUP BY orders_id
+SELECT
+    o.orders_id,
+    o.date_date,
+    ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost), 2) AS operational_margin,
+    o.quantity,
+    o.revenue,
+    o.purchase_cost,
+    o.margin,
+    s.shipping_fee,
+    s.logcost,
+    s.ship_cost
+FROM {{ ref("int_orders_margin") }} o
+LEFT JOIN {{ ref("stg_raw__ship") }} s
+USING (orders_id)
+ORDER BY orders_id DESC
